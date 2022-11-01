5433986d80e846770dec6bc492d451789a19dde6
付强 <fuqiang1@CN0214003515M.lan>
2022-11-01 12:50:09 +0800

    feat: database

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,7 +14,9 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { BusinessException } from 'src/common/exceptions/business.exception';
 import { ConfigService } from '@nestjs/config';
-
+import { AddUserDto } from './user.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('用户')
 @Controller({
   path: 'user',
   version: '1',
@@ -24,59 +26,25 @@ export class UserController {
     private readonly userService: UserService,
     private readonly configService: ConfigService,
   ) {}
-
+  @ApiOperation({
+    summary: '新增用户',
+  })
+  @Post('/add')
+  create(@Body() user: AddUserDto) {
+    return this.userService.createOrSave(user);
+  }
   @Get('getTestName')
   getTestName() {
     return this.configService.get('TEST_VALUE').name;
   }
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
-  @Get()
-  @Version([VERSION_NEUTRAL, '1'])
-  findAll() {
-    return this.userService.findAll();
-  }
-  @Get('findError')
-  @Version([VERSION_NEUTRAL, '1'])
-  findError() {
-    const a: any = {};
-    console.log(a.b.c);
-    return this.userService.findAll();
-  }
-
-  @Get('findBusinessError')
-  @Version([VERSION_NEUTRAL, '1'])
-  findBusinessError() {
-    const a: any = {};
-    try {
-      console.log(a.b.c);
-    } catch (error) {
-      // throw new BusinessException('参数错误');
-      throw BusinessException.throwForbidden();
-    }
-    return this.userService.findAll();
-  }
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.userService.create(createUserDto);
+  // }
 
   @Get()
   @Version('2')
   findAll2() {
     return 'another one';
-  }
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,16 @@
-import { Module, CacheModule, forwardRef } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
+import { DatabaseModule } from '@/common/database/database.module';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserProviders } from './user.providers';
+
 import { FeishuService } from './feishu/feishu.service';
 import { FeishuController } from './feishu/feishu.controller';
 
 @Module({
-  imports: [CacheModule.register()],
-  controllers: [FeishuController],
-  providers: [FeishuService],
+  imports: [DatabaseModule],
+  controllers: [UserController, FeishuController],
+  providers: [...UserProviders, UserService, FeishuService],
+  exports: [UserService],
 })
 export class UserModule {}
